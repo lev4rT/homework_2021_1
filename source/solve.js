6a955cd3e00c435a176f59bc8f96fe35cf6f532c
Denis <kdv.levart@gmail.com>
2021-02-06 17:07:34 +0300

    Kolosov Denis. Variant 18.

---
+++ source/solve.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const solve = (equation, x) => eval(equation.replaceAll("x", x));
--- tests/solve.js
+++ tests/solve.js
@@ -8,5 +8,9 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('2 * ( x - 1 )', 5), 8);
 		assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
 		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
+
+		// Additional tests:
+		assert.strictEqual(solve('(x + 3 - x * 4) + x * x', 2), 1);
+		assert.strictEqual(solve('(x + 2) * (x - 4) + 2 * (x - 1)', 3), -1);
 	});
 });
