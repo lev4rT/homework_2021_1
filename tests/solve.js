1ff116489e6292731334badee44270337919cbf1
Denis <kdv.levart@gmail.com>
2021-02-07 18:15:08 +0300

    New tests were added.

--- tests/solve.js
+++ tests/solve.js
@@ -12,5 +12,23 @@ QUnit.module('Тестируем функцию solve', function () {
 		// Additional tests:
 		assert.strictEqual(solve('(x + 3 - x * 4) + x * x', 2), 1);
 		assert.strictEqual(solve('(x + 2) * (x - 4) + 2 * (x - 1)', 3), -1);
+
+		// And another tests:
+		assert.strictEqual(solve('(x - 2) * 3', 4), 6);
+		assert.strictEqual(solve('(x + 2) * 3', 4), 18);
+		assert.strictEqual(solve('(x * 3) - (2 * x + 2)', 2), 0);
+		assert.strictEqual(solve('(x + 2) * 0', 5), 0);
+
+		// Incorrect expression strings tests:
+		// Incorrect close bracket
+		assert.strictEqual(solve('x + 1 )', 2), undefined);
+		// Incorrect open bracket
+		assert.strictEqual(solve('(x + 1', 2), undefined);
+		// Open brackets amount != close brackets amount
+		assert.strictEqual(solve('(x + 1) + 3 )', 2), undefined);
+		// Close brackets amount != open brackets amount
+		assert.strictEqual(solve('((x + 1) + 3', 2), undefined);
+		// Undefined 'y' parameter was given
+		assert.strictEqual(solve('(x + 1) + 3 * y', 2), undefined);
 	});
 });
