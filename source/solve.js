8e5ae92aaae84de7fba7223e24bff16cd62946d1
Denis <kdv.levart@gmail.com>
2021-02-07 18:04:38 +0300

    jsdoc was added.

--- source/solve.js
+++ source/solve.js
@@ -1,3 +1,17 @@
 'use strict';
 
+/**
+ * Solves expressions of the form (x + 2) + (x - 2) + (x * 2) with a given x parameter.
+ * Supported operations: '+', '-', '*'.
+ * @example
+ * // returns 3
+ * solve('x + 2', 1);
+ * @example
+ * // returns 4
+ * solve('x * 2', 2);
+ * @example
+ * // returns 1
+ * solve('x - 2', 3);
+ * @returns {Number} Returns the result of an expression with a substituted value x.
+ */
 const solve = (equation, x) => eval(equation.replaceAll("x", x));
