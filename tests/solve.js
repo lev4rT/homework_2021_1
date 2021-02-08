6746c8465469f9b3c432c66452de4a3db171f5c1
Denis <kdv.levart@gmail.com>
2021-02-09 00:38:37 +0300

    Some new tests. Tests are now looks much better.

--- tests/solve.js
+++ tests/solve.js
@@ -8,27 +8,30 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('2 * ( x - 1 )', 5), 8);
 		assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
 		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
+	});
 
-		// Additional tests:
+	QUnit.test('solve работает правильно. Первый доп. тесты ', function (assert) {
 		assert.strictEqual(solve('(x + 3 - x * 4) + x * x', 2), 1);
 		assert.strictEqual(solve('(x + 2) * (x - 4) + 2 * (x - 1)', 3), -1);
+	});
 
-		// And another tests:
+	QUnit.test('solve работает правильно. Вторые доп. тесты ', function (assert) {
 		assert.strictEqual(solve('(x - 2) * 3', 4), 6);
 		assert.strictEqual(solve('(x + 2) * 3', 4), 18);
 		assert.strictEqual(solve('(x * 3) - (2 * x + 2)', 2), 0);
 		assert.strictEqual(solve('(x + 2) * 0', 5), 0);
+	});
 
-		// Incorrect expression strings tests:
-		// Incorrect close bracket
-		assert.strictEqual(solve('x + 1 )', 2), undefined);
-		// Incorrect open bracket
-		assert.strictEqual(solve('(x + 1', 2), undefined);
-		// Open brackets amount != close brackets amount
-		assert.strictEqual(solve('(x + 1) + 3 )', 2), undefined);
-		// Close brackets amount != open brackets amount
-		assert.strictEqual(solve('((x + 1) + 3', 2), undefined);
-		// Undefined 'y' parameter was given
-		assert.strictEqual(solve('(x + 1) + 3 * y', 2), undefined);
+	QUnit.test('solve работает правильно. Обработка случаев некорректного количества скобок ', function (assert) {
+		assert.throws(() => solve('x + 1 )', 2), SyntaxError);
+		assert.throws(() => solve('(x + 1', 2), SyntaxError);
+		assert.throws(() => solve('(x + 1) + 3 )', 2), SyntaxError);
+		assert.throws(() => solve('((x + 1) + 3', 2), SyntaxError);
+	});
+
+	QUnit.test('solve работает правильно. Обработка случаев некорректного символа в выражении ', function (assert) {
+		assert.throws(() => solve('(x + 1) + 3 * y', 2), SyntaxError);
+		assert.throws(() => solve('x / 2', 2), SyntaxError);
+		assert.throws(() => solve('x % 2', 2), SyntaxError);
 	});
 });
