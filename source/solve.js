719d59c10236d348244b84ff63bcb5297508fe2d
Denis <kdv.levart@gmail.com>
2021-02-08 23:41:56 +0300

    Additional function description was added.

--- source/solve.js
+++ source/solve.js
@@ -3,6 +3,8 @@
 /**
  * Solves expressions of the form (x + 2) + (x - 2) + (x * 2) with a given x parameter.
  * Supported operations: '+', '-', '*'.
+ * @param {String} expression - expression to be calculated.
+ * @param {Number} x - the value to be substituted into the expression.
  * @example
  * // returns 3
  * solve('x + 2', 1);
@@ -12,12 +14,13 @@
  * @example
  * // returns 1
  * solve('x - 2', 3);
- * @returns {Number} Returns the result of an expression with a substituted value x.
+ * @returns {Number} Result of an expression with a substituted value x (if expression is correct).
+ * @returns {undefined} If expression is incorrect - function returns undefined.
  */
 const solve = (expression, x) => {
-	let result = undefined;
+	let result;
 	try {
-		result = eval(expression.replaceAll("x", x));
+		result = eval(expression.replaceAll('x', x));
 	} catch (error) { ; }
 
 	return result;
