8580a58b49a552d893dc7a32915810af78db2720
Denis <kdv.levart@gmail.com>
2021-02-09 19:50:35 +0300

    Rudimental QUnits were merged.

--- tests/solve.js
+++ tests/solve.js
@@ -8,14 +8,8 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('2 * ( x - 1 )', 5), 8);
 		assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
 		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
-	});
-
-	QUnit.test('solve работает правильно. Первый доп. тесты ', function (assert) {
 		assert.strictEqual(solve('(x + 3 - x * 4) + x * x', 2), 1);
 		assert.strictEqual(solve('(x + 2) * (x - 4) + 2 * (x - 1)', 3), -1);
-	});
-
-	QUnit.test('solve работает правильно. Вторые доп. тесты ', function (assert) {
 		assert.strictEqual(solve('(x - 2) * 3', 4), 6);
 		assert.strictEqual(solve('(x + 2) * 3', 4), 18);
 		assert.strictEqual(solve('(x * 3) - (2 * x + 2)', 2), 0);
